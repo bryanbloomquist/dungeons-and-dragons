2d4033df0f6968059826ec2ecbbc8289ea2c0490
bloomy215 <bmbloomquist@gmail.com>
2020-01-21 13:01:13 -0600

    i lied, i wasn't done styling the accordion form, i am now though, i promise

--- src/Components/AccordionMenu/AccordionMenu.js
+++ src/Components/AccordionMenu/AccordionMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Accordion, Card, Button } from "react-bootstrap";
+// import { Accordion, Card, Button } from "react-bootstrap";
+import { Accordion, Card, Button, Row, Col } from "react-bootstrap";
 import DiceRoller from "./DiceRoller";
 import AddNPC from "./AddNPC";
 import AddPC from "./AddPC";
@@ -8,15 +9,23 @@ const AccordionMenu = () => {
 	return (
 		<Accordion>
 			<Card>
-				<Accordion.Toggle as={Button} variant="warning" className="m-2" eventKey="0">
-					Roll Dice
-				</Accordion.Toggle>
-				<Accordion.Toggle as={Button} variant="warning" className="m-2" eventKey="1">
-					Add NPC
-				</Accordion.Toggle>
-				<Accordion.Toggle as={Button} variant="warning" className="m-2" eventKey="2">
-					Add PC
-				</Accordion.Toggle>
+				<Row>
+					<Col sm={4} className="px-1">
+						<Accordion.Toggle as={Button} variant="warning" className="mb-2" eventKey="0" block>
+							Dice
+						</Accordion.Toggle>
+					</Col>
+					<Col sm={4} className="px-1">
+						<Accordion.Toggle as={Button} variant="warning" className="mb-2" eventKey="1" block>
+							NPC
+						</Accordion.Toggle>
+					</Col>
+					<Col sm={4} className="px-1">
+						<Accordion.Toggle as={Button} variant="warning" className="mb-2" eventKey="2" block>
+							PC
+						</Accordion.Toggle>
+					</Col>
+				</Row>
 				<Accordion.Collapse eventKey="0">
 					<Card.Body className="p-0">
 						<h1>Roll Dice</h1>
