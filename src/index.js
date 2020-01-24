429dc5567478bb5d7555725e111767d88cc405a2
Bryan Bloomquist <bmbloomquist@gmail.com>
2020-01-23 22:32:58 -0600

    connected to firebase, i think, fingers crossed, good night

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Firebase from "./Components/Firebase/Firebase";
+import FirebaseContext from "./Components/Firebase/Context";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
+);
